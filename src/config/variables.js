a4e5216fc772ee0847c445ae61e5ff5fd1c3228a
Md. Nahid <nahidjc80@gmail.com>
2024-04-24 11:12:49 +0600

    mongodb packege added

--- src/config/variables.js
+++ src/config/variables.js
@@ -2,7 +2,6 @@ const PORT = process.env.PORT;
 const DB_URL = process.env.MONGODB_URL;
 const NODE_ENV = process.env.NODE_ENV;
 const jwtSecret = process.env.JWT_SECRET;
-console.log(DB_URL);
 module.exports = {
   PORT,
   DB_URL,
