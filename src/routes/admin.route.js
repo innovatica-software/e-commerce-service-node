6c57303caac7f953996a83cd3c08d97b0fe87103
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-04-28 11:03:33 +0600

    admin login functionality removed

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,9 +1,6 @@
 const { Router } = require("express");
-const { adminLogin } = require("../controllers/admin.controller");
 const { getAllUsers } = require("../models/adminAuth");
 
 const router = Router();
-router.post("/login", adminLogin);
 router.get("/getuserlist", getAllUsers);
-
 module.exports = router;
