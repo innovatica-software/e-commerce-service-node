12ef4deffacdc95148a40d8f770255ded3e51e4a
MasudRana15924 <masudrana15924@outlook.com>
2024-05-08 00:07:45 +0600

    Product image upload in multer and cloudinary

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,11 +1,31 @@
+const { log } = require("console");
 const { errorResponseHandler } = require("../helper/errorResponseHandler");
 const ProductModel = require("../models/product");
-
+const cloudinary = require("cloudinary");
+const fs = require("fs");
+cloudinary.config({
+  cloud_name:process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 const createProduct = async (req, res) => {
   try {
     const { name, price, description, category, brand, countInStock } =
       req.body;
     const { id } = req.user;
+    let result = {};
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+    if (req.file) {
+      const file = req.file;
+      result = await cloudinary.uploader.upload(file.path, options);
+      fs.unlink(file.path, function (err) {
+        if (err) throw err;
+      });
+    }
     const newProduct = await ProductModel.createProduct({
       name,
       price,
@@ -14,7 +34,9 @@ const createProduct = async (req, res) => {
       brand,
       countInStock,
       userId: id,
+      image : result.url
     });
+    console.log("result.url",result.url);
     res.created(newProduct, "Product created successfully ");
   } catch (err) {
     errorResponseHandler(err, req, res);
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,8 +3,15 @@ const {
   createProduct,
   getProducts,
 } = require("../controllers/product.controller");
+const multer = require('multer')
+const upload = multer({
+  dest: "tmp/",
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  }
+});
 const { adminAuthenticate } = require("../middleware/authenticate");
 const router = Router();
-router.post("/create", adminAuthenticate, createProduct);
+router.post("/create", adminAuthenticate,  upload.single("image"), createProduct);
 router.get("/", getProducts);
 module.exports = router;
