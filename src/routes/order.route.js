8815de7f09b923d8e6e455a10f40c6eface6078c
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-05-16 22:13:46 +0600

    added order retrieve functionality for user

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const { userAuthenticate } = require("../middleware/authenticate");
-const { createOrder } = require("../controllers/order.controller");
+const {
+  createOrder,
+  getAllOrders,
+} = require("../controllers/order.controller");
 
 const router = Router();
 router.post("/add", userAuthenticate, createOrder);
-
+router.get("/my/orders", userAuthenticate, getAllOrders);
 
 module.exports = router;
